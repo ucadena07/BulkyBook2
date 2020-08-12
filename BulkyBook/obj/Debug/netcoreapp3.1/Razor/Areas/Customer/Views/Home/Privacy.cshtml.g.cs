fe5c490ee3cc0b2e0b41a934aa4a819566303a0b
DESKTOP-56F9530\ucadena07 <ulises@netkinetix.com>
2020-08-11 22:12:35 -0500

    created admin area

--- BulkyBook/obj/Debug/netcoreapp3.1/Razor/Areas/Customer/Views/Home/Privacy.cshtml.g.cs
+++ BulkyBook/obj/Debug/netcoreapp3.1/Razor/Areas/Customer/Views/Home/Privacy.cshtml.g.cs
@@ -12,7 +12,22 @@ namespace AspNetCore
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.AspNetCore.Mvc.ViewFeatures;
+#nullable restore
+#line 1 "C:\Users\ucadena07\Documents\MVC\BulkyBook\BulkyBook\Areas\Customer\Views\_ViewImports.cshtml"
+using BulkyBook;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 2 "C:\Users\ucadena07\Documents\MVC\BulkyBook\BulkyBook\Areas\Customer\Views\_ViewImports.cshtml"
+using BulkyBook.Models;
+
+#line default
+#line hidden
+#nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"d8ddb6bffa5a9b264bf8f89038bf03c234083fd3", @"/Areas/Customer/Views/Home/Privacy.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"2b283f6c8c7163b5f5d1f909d7b66a1ed4e3ccc1", @"/Areas/Customer/Views/_ViewImports.cshtml")]
     public class Areas_Customer_Views_Home_Privacy : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
