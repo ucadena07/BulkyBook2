c4866483f70a7546677b6dfd17b6e450c3b3b1e0
DESKTOP-56F9530\ucadena07 <ulises@netkinetix.com>
2020-08-11 22:01:26 -0500

    creating customer area

--- BulkyBook/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBook/Areas/Customer/Controllers/HomeController.cs
@@ -6,9 +6,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 
-namespace BulkyBook.Controllers
+namespace BulkyBook.Areas.CUstomer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
