fa550e4f8a9da3de95a205833094cf6a2b7068e9
DESKTOP-56F9530\ucadena07 <ulises@netkinetix.com>
2020-08-15 10:49:49 -0500

    Category CRUD part 3

--- BulkyBook/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyBook/Areas/Admin/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
@@ -42,12 +43,50 @@ namespace BulkyBook.Areas.Admin.Controllers
 
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                if (category.Id == 0)
+                {
+                    _unitOfWork.Category.Add(category);
+                    
+                }
+                else
+                {
+                    _unitOfWork.Category.Update(category);
+                    
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+
+            }
+            return View(category);
+        }
+
+
         #region API CALLS
         [HttpGet]
         public IActionResult GetAll()
         {
             var allObj = _unitOfWork.Category.GetAll();
             return Json(new { data = allObj });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Category.Get(id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _unitOfWork.Category.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete succesful" });
+
         }
         #endregion
     }
